5a49e452f7ef98c2553dc22df9d8350aafb74d0d
Mia1251 <svanteomax@gmail.com>
2021-03-09 09:29:56 +0100

    AppUserTest completed and ran successfully

---
+++ src/test/java/se/lexicon/jpa_workshop/entity/AppUserTest.java
@@ -0,0 +1,40 @@
+package se.lexicon.jpa_workshop.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppUserTest {
+
+    AppUser testObject;
+
+    @BeforeEach
+    public void setup() {
+        testObject = new AppUser(1, "Andreas", "Svensson", "andreas.svensson@gmail.com");
+    }
+
+    @Test
+    @DisplayName("Test1 created new object")
+    public void test_create() {
+        Assertions.assertEquals(1,testObject.getId());
+        Assertions.assertEquals("Andreas",testObject.getFirstName());
+        Assertions.assertEquals("Svensson",testObject.getLastName());
+        Assertions.assertEquals("andreas.svensson@gmail.com",testObject.getEmail());
+    }
+
+    @Test
+    @DisplayName("Test2 Egual")
+    public void test_equal() {
+        AppUser expected = new AppUser(1, "Andreas", "Svensson", "andreas.svensson@gmail.com");
+        Assertions.assertTrue(testObject.equals(expected));
+    }
+
+    @Test
+    @DisplayName("Test3 HashCode")
+    public void test_hashCode() {
+        AppUser expected = new AppUser(1, "Andreas", "Svensson", "andreas.svensson@gmail.com");
+        Assertions.assertEquals(expected.hashCode(), testObject.hashCode());
+    }
+
+}
